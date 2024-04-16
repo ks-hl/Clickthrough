989926acf73c5f0b5a5b3951f17d011bdaafea95
Heliosares <kyler.t.skeens@gmail.com>
2024-04-15 20:20:51 -0400

    Remove unnecessary import

--- src/dev/heliosares/clickthrough/MyListener.java
+++ src/dev/heliosares/clickthrough/MyListener.java
@@ -19,7 +19,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerSignOpenEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
